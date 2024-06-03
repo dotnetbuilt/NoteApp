a7ee06d63f6235c7b7961ad0c20b8297efbdefbb
Abbos Zoirov <122oszoirov@gmail.com>
2024-06-03 22:43:16 +0500

    add Note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{' '.join(self.content.split()[:3])}...'
+
